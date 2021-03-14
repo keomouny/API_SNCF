6fa20bc826cb68e18ef5dbb46cb4ae128718c269
keomouny <mouny.keo@hetic.net>
2021-03-14 23:09:58 +0100

    updated

--- utils/setup_logger.py
+++ utils/setup_logger.py
@@ -1,6 +1,7 @@
 import logging
+import os
 
-logging.basicConfig(filename="logs/log.log", filemode="a",
+logging.basicConfig(filename=f"{os.getcwd()}logs/log.log", filemode="a",
                     format="%(asctime)s - %(levelname)-8s - %(message)s", datefmt="%d/%m/%Y %H:%M:%S", level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
